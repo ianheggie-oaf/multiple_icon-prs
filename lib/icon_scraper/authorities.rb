a613b19da1e8661de722f2a58c5e8287c65479d2
Taz17 <mutaz.ashhab@gmail.com>
2022-01-24 14:39:52 +1100

    Updated Northern Beaches council period

--- lib/icon_scraper/authorities.rb
+++ lib/icon_scraper/authorities.rb
@@ -91,7 +91,7 @@ module IconScraper
     },
     northern_beaches: {
       url: "https://eservices.northernbeaches.nsw.gov.au/ePlanning/live/Public/XC.Track",
-      period: "last14days",
+      period: "thismonth",
       types: ["DevApp"]
     },
     penrith: {
