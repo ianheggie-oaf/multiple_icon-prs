0f65070be97d8597454ec6a8a509e72527f9fc85
Matthew Landauer <mlandauer@gmail.com>
2022-06-20 15:08:51 +1000

    Put all authorities in alpha order to make things easier to find

--- lib/icon_scraper/authorities.rb
+++ lib/icon_scraper/authorities.rb
@@ -2,59 +2,48 @@
 
 module IconScraper
   AUTHORITIES = {
-    strathfield: {
-      url: "http://daenquiry.strathfield.nsw.gov.au/Pages/XC.Track",
-      period: "last28days"
+    bayside: {
+      url: "https://eplanning.bayside.nsw.gov.au/ePlanning/Pages/XC.Track",
+      period: "thismonth",
+      types: [217]
     },
-    mackay: {
-      url: "https://planning.mackay.qld.gov.au/Pages/XC.Track",
-      period: "last28days"
+    blue_mountains: {
+      url: "https://www2.bmcc.nsw.gov.au/DATracking/Pages/XC.Track",
+      period: "last14days"
     },
-    georges_river: {
-      url: "https://etrack.georgesriver.nsw.gov.au/Pages/XC.Track",
-      period: "thismonth"
+    boroondara: {
+      url: "https://eservices.boroondara.vic.gov.au/EPlanning/Pages/XC.Track",
+      period: "thismonth",
+      types: %w[PlnPermit PlnAppeals PlnPostPer PlanPermGr PlanAmend PlanAppeal]
     },
-    kyogle: {
-      url: "https://etrack.kyogle.nsw.gov.au/Pages/XC.Track",
-      period: "last28days"
-    },
-    greater_hume: {
-      url: "http://datracker.greaterhume.nsw.gov.au/Pages/XC.Track",
-      period: "last28days"
-    },
-    richmond_valley: {
-      url: "http://datracker.richmondvalley.nsw.gov.au/Pages/XC.Track",
-      period: "last28days"
+    canada_bay: {
+      url: "http://datracking.canadabay.nsw.gov.au/Pages/XC.Track",
+      period: "last14days"
     },
     central_highlands: {
       url: "https://track.chrc.qld.gov.au/Pages/XC.Track",
       period: "last28days",
       types: [205, 400, 401, 402, 403, 405]
     },
-    whitsunday: {
-      url: "http://eplanning.whitsundayrc.qld.gov.au/Pages/XC.Track",
-      period: "last28days"
+    coffs_harbour: {
+      url: "https://chcc-icon.saas.t1cloud.com/public/Pages/xc.Track",
+      period: "last14days"
     },
     cumberland: {
       url: "https://cumberland-eplanning.t1cloud.com/Pages/XC.Track",
       period: "last14days"
     },
-    coffs_harbour: {
-      url: "https://chcc-icon.saas.t1cloud.com/public/Pages/xc.Track",
-      period: "last14days"
-    },
-    blue_mountains: {
-      url: "https://www2.bmcc.nsw.gov.au/DATracking/Pages/XC.Track",
-      period: "last14days"
-    },
-    swan: {
-      url: "https://elodge.swan.wa.gov.au/Pages/XC.Track",
-      types: [282, 281, 283],
-      period: "thisweek"
+    georges_river: {
+      url: "https://etrack.georgesriver.nsw.gov.au/Pages/XC.Track",
+      period: "thismonth"
     },
     gosnells: {
       url: "http://apps.gosnells.wa.gov.au/ICON/Pages/XC.Track",
       period: "last14days"
+    },
+    greater_hume: {
+      url: "http://datracker.greaterhume.nsw.gov.au/Pages/XC.Track",
+      period: "last28days"
     },
     hobart: {
       url: "https://apply.hobartcity.com.au/Pages/XC.Track",
@@ -66,6 +55,10 @@ module IconScraper
       period: "last14days",
       ssl_verify: false
     },
+    kyogle: {
+      url: "https://etrack.kyogle.nsw.gov.au/Pages/XC.Track",
+      period: "last28days"
+    },
     leichhardt: {
       url: "http://www.eservices.lmc.nsw.gov.au/ApplicationTracking/Pages/XC.Track",
       period: "last14days",
@@ -75,19 +68,23 @@ module IconScraper
       url: "https://eplanning.liverpool.nsw.gov.au/Pages/XC.Track",
       period: "last14days"
     },
+    mackay: {
+      url: "https://planning.mackay.qld.gov.au/Pages/XC.Track",
+      period: "last28days"
+    },
     mosman: {
       url: "http://portal.mosman.nsw.gov.au/Pages/XC.Track",
       period: "last14days",
       types: [8, 5]
     },
-    north_sydney: {
-      url: "https://apptracking.northsydney.nsw.gov.au/Pages/XC.Track",
-      period: "last14days"
-    },
     northern_beaches: {
       url: "https://eservices.northernbeaches.nsw.gov.au/ePlanning/live/Public/XC.Track",
       period: "thismonth",
       types: ["DevApp"]
+    },
+    north_sydney: {
+      url: "https://apptracking.northsydney.nsw.gov.au/Pages/XC.Track",
+      period: "last14days"
     },
     penrith: {
       url: "http://bizsearch.penrithcity.nsw.gov.au/ePlanning/Pages/XC.Track",
@@ -107,19 +104,36 @@ module IconScraper
         MCSS OP EC SB SBSS PD BX ROL QRAL
       ]
     },
-    bayside: {
-      url: "https://eplanning.bayside.nsw.gov.au/ePlanning/Pages/XC.Track",
-      period: "thismonth",
-      types: [217]
+    richmond_valley: {
+      url: "http://datracker.richmondvalley.nsw.gov.au/Pages/XC.Track",
+      period: "last28days"
     },
     scenic_rim: {
       url: "https://srr-prod-icon.saas.t1cloud.com/Pages/XC.Track",
       period: "last28days"
     },
+    strathfield: {
+      url: "http://daenquiry.strathfield.nsw.gov.au/Pages/XC.Track",
+      period: "last28days"
+    },
+    swan: {
+      url: "https://elodge.swan.wa.gov.au/Pages/XC.Track",
+      types: [282, 281, 283],
+      period: "thisweek"
+    },
+    tweed: {
+      url: "https://s1.tweed.nsw.gov.au/Pages/XC.Track",
+      period: "thismonth",
+      types: %w[DA CDC]
+    },
     waverley: {
       url: "https://eservices.waverley.nsw.gov.au/Pages/XC.Track",
       period: "last14days",
       types: %w[A0 SP2A TPO B1 B1A FPS]
+    },
+    whitsunday: {
+      url: "http://eplanning.whitsundayrc.qld.gov.au/Pages/XC.Track",
+      period: "last28days"
     },
     willoughby: {
       url: "https://eplanning.willoughby.nsw.gov.au/pages/xc.track",
@@ -130,20 +144,6 @@ module IconScraper
         "cd01b", "cd02", "cd04", "bcertu", "bcertr", "bcertc",
         "tvpa", "tvpa 2", "tvpa r"
       ]
-    },
-    canada_bay: {
-      url: "http://datracking.canadabay.nsw.gov.au/Pages/XC.Track",
-      period: "last14days"
-    },
-    tweed: {
-      url: "https://s1.tweed.nsw.gov.au/Pages/XC.Track",
-      period: "thismonth",
-      types: %w[DA CDC]
-    },
-    boroondara: {
-      url: "https://eservices.boroondara.vic.gov.au/EPlanning/Pages/XC.Track",
-      period: "thismonth",
-      types: %w[PlnPermit PlnAppeals PlnPostPer PlanPermGr PlanAmend PlanAppeal]
     }
   }.freeze
 end
